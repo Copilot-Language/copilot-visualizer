0fcb73ccea950032896418a8fdce7fdf7efaf40e
Ivan Perez <ivan.perezdominguez@nasa.gov>
2025-02-17 18:11:39 -0800

    Cleaning

--- src/Copilot/Visualize.hs
+++ src/Copilot/Visualize.hs
@@ -5,18 +5,62 @@
 module Copilot.Visualize
   where
 
-import Copilot.Core
-import Copilot.Interpret.Eval
+-- External imports
 import Data.Aeson
 import Data.List
-import Data.Maybe               ( fromMaybe, isJust, isNothing )
+import Data.Maybe             ( fromMaybe, isJust, isNothing )
 import GHC.Generics
-import Paths_copilot_visualizer
 import System.Directory
 import System.Directory.Extra
 import System.FilePath
 import Text.Printf
 import Text.Read
+
+-- External imports: Copilot
+import Copilot.Core
+import Copilot.Interpret.Eval
+
+-- Internal imports
+import Paths_copilot_visualizer
+
+-- | Generate a visualization of a specification for a given number of steps.
+makeTrace :: Int     -- ^ Number of steps to interpret.
+          -> Spec    -- ^ Specification to interpret.
+          -> IO ()
+makeTrace k spec = do
+    dir <- getDataDir
+    let f = dir </> "data"
+    let subs = toJSON $ makeTraceEval k spec e
+    copyTemplate f subs "target"
+  where
+    e = eval Haskell k spec
+
+-- | Generate an abstract representation of a trace of a specification
+-- interpreted for a given number of steps.
+makeTraceEval :: Int
+              -> Spec
+              -> ExecTrace
+              -> AppData
+makeTraceEval k spec e =
+    AppData (observerTEs ++ triggerTEs) (k - 1)
+  where
+    observerTEs = map mkTraceElem (interpObserversOpt spec e)
+    triggerTEs  = map mkTraceElem (interpTriggersWithArgs spec e)
+
+mkTraceElem :: (String, [Maybe Output]) -> TraceElem
+mkTraceElem (name, outputs) = TraceElem
+    { teName      = name
+    , teValues    = values
+    , teIsBoolean = any (isBoolean . tvValue) values
+    , teIsFloat   = any (isFloat . tvValue) values
+    }
+  where
+    values = map mkTraceValue outputs
+
+mkTraceValue :: Maybe Output -> TraceValue
+mkTraceValue x = TraceValue (showValue x) (isNothing x)
+
+-- * Abstract representation of a trace
 
 data AppData = AppData
     { adTraceElems :: Trace
@@ -39,41 +83,24 @@ data TraceElem = TraceElem
 instance ToJSON TraceElem
 
 data TraceValue = TraceValue
-    { tvValue     :: String
-    , tvIsEmpty   :: Bool
+    { tvValue   :: String
+    , tvIsEmpty :: Bool
     }
   deriving (Generic, Show)
 
 instance ToJSON TraceValue
 
-makeTrace :: Int     -- ^ Number of steps to interpret.
-          -> Spec    -- ^ Specification to interpret.
-          -> IO ()
-makeTrace k spec = do
-    dir <- getDataDir
-    let f = dir </> "data"
-    let subs = toJSON $ makeTraceEval k spec e
-    copyTemplate f subs "target"
-  where
-    e = eval Haskell k spec
+-- * Auxiliary functions
 
-makeTraceEval :: Int
-              -> Spec
-              -> ExecTrace
-              -> AppData
-makeTraceEval k spec e =
-    AppData (observerTEs ++ triggerTEs) (k - 1)
-  where
-    observerTEs = map mkTraceElem (interpObserversOpt spec e)
-    triggerTEs  = map mkTraceElem (interpTriggersWithArgs spec e)
-
+-- | Compute the list of values associated to observers.
 interpObserversOpt :: Spec -> ExecTrace -> [(String, [Maybe Output])]
-interpObserversOpt spec e =
+interpObserversOpt _spec e =
   map (\(n, os) -> (n, map Just os)) (interpObservers e)
 
--- Compute the list of values associated to a trigger and its arguments, which
--- the first values being the ones for the trigger itself, and subsequent
--- values being those of the arguments to the trigger.
+-- | Compute the list of values associated to triggers and their arguments.
+--
+-- For each trigger, we first include the values of the trigger itself, and
+-- then the values of the arguments to the trigger.
 interpTriggersWithArgs :: Spec -> ExecTrace -> [(String, [Maybe Output])]
 interpTriggersWithArgs spec e = concatMap triggerOutputs (interpTriggers e)
   where
@@ -84,26 +111,25 @@ interpTriggersWithArgs spec e = concatMap triggerOutputs (interpTriggers e)
       where
         triggerValues = map triggerValue triggerArgs
 
-        triggerValue :: Maybe a -> Maybe Output
+	-- Value for the trigger at a given time, based on the values of its
+        -- arguments.
+        triggerValue :: Maybe [Output] -> Maybe Output
         triggerValue Nothing  = Just "false"
         triggerValue (Just _) = Just "true"
 
+        -- Names and values for the arguments.
         argNames  = map (\ix -> triggerName ++ "Arg" ++ show ix) [0..]
-        argValues = transpose (transMaybes triggerArgs numArgs)
+        argValues = transpose (transMaybes numArgs triggerArgs)
         numArgs   = triggerNumArgs spec triggerName
 
-mkTraceElem :: (String, [Maybe Output]) -> TraceElem
-mkTraceElem (name, outputs) = TraceElem
-    { teName      = name
-    , teValues    = values
-    , teIsBoolean = any (isBoolean . tvValue) values
-    , teIsFloat   = any (isFloat . tvValue) values
-    }
-  where
-    values = map mkTraceValue outputs
-
-mkTraceValue :: Maybe Output -> TraceValue
-mkTraceValue x = TraceValue (showValue x) (isNothing x)
+-- Number of arguments to a trigger in a spec.
+--
+-- PRE: name exists as a trigger in spec.
+triggerNumArgs :: Spec -> String -> Int
+triggerNumArgs spec name =
+  case find (\t -> triggerName t == name) (specTriggers spec) of
+    Nothing -> error "Couldn't find given trigger in spec, should never occur!"
+    Just t  -> length $ triggerArgs t
 
 -- | True if the input value denotes a boolean value.
 isBoolean :: String -> Bool
@@ -128,27 +154,17 @@ showValue Nothing  = "--"
 showValue (Just s) | isFloat s = showValueFloat s
                    | otherwise = s
 
+-- | Show a floating point value.
 showValueFloat :: Output -> String
 showValueFloat = formatFloat . read
   where
     formatFloat :: Double -> String
     formatFloat = printf "%.2g"
 
--- * Auxiliary functions
-
--- Number of arguments to a trigger in a spec.
---
--- PRE: name exists as a trigger in spec.
-triggerNumArgs :: Spec -> String -> Int
-triggerNumArgs spec name =
-  case find (\t -> triggerName t == name) (specTriggers spec) of
-    Nothing -> error "Couldn't find given trigger in spec, should never occur!"
-    Just t  -> length $ triggerArgs t
-
 -- | Given a list of maybe lists of known length, this function creates a list
 -- of lists, pushing the Maybe's inside.
-transMaybes :: [Maybe [a]] -> Int -> [[Maybe a]]
-transMaybes []       _          = []
-transMaybes (xs:xss) argsLength = case xs of
-  Just xs' -> map Just xs' : transMaybes xss argsLength
-  Nothing  -> replicate argsLength Nothing : transMaybes xss argsLength
+transMaybes :: Int -> [Maybe [a]] -> [[Maybe a]]
+transMaybes = map . transMaybes'
+  where
+    transMaybes' argsLength (Just xs) = map Just xs
+    transMaybes' argsLength Nothing   = replicate argsLength Nothing
